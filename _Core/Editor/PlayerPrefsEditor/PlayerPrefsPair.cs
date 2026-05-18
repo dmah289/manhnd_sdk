c585a021a3e793581d625a4b359c0bbda962bf33
dmah <manhnd@ikameglobal.com>
2026-05-18 17:18:30 +0700

    sync

--- _Core/Editor/PlayerPrefsEditor/PlayerPrefsPair.cs
+++ _Core/Editor/PlayerPrefsEditor/PlayerPrefsPair.cs
@@ -1,8 +1,35 @@
-﻿namespace manhnd_sdk.Editor.PlayerPrefsEditor
+﻿using System;
+using UnityEngine;
+
+namespace manhnd_sdk.Editor.PlayerPrefsEditor
 {
     public struct PlayerPrefsPair
     {
+        private static string AliasInt = "int";
+        private static string AliasFloat = "float";
+        private static string AliasString = "string";
+        
         public string Key;
         public object Value;
+        
+        public Color TypeColor =>
+            Value switch
+            {
+                int => Color.cyan,
+                float => Color.magenta,
+                string => Color.green,
+                _ => Color.white
+            };
+        
+        public string AliasType =>
+            Value switch
+            {
+                int => AliasInt,
+                float => AliasFloat,
+                string => AliasString,
+                _ => string.Empty
+            };
+
+        public Type ValueType => Value.GetType();
     }
 }
