5ce505c5e5c4f95b53cd1bcd21a7e90c8b7bcd85
manhnd <manhnd@ikameglobal.com>
2026-05-31 22:21:39 +0700

    sync

---
+++ _Core/Runtime/SystemDesign/Singleton/MonoSingleton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace manhnd_sdk.Runtime.SystemDesign
+{
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T instance;
+        public static T Instance => instance;
+
+        protected virtual void Awake()
+        {
+            if (instance != null && instance != this)
+                Destroy(gameObject);
+            else
+            {
+                instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+    }
+}
---
+++ _Core/Runtime/SystemDesign/Singleton/SingletonSO.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace manhnd_sdk.Runtime.SystemDesign
+{
+    public abstract class SingletonSO<T> : ScriptableObject where T : ScriptableObject
+    {
+        private static T _instance;
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    // Try to find an existing instance in the project
+                    _instance = Resources.Load<T>(typeof(T).Name);
+                    if (_instance == null)
+                    {
+                        Debug.LogError($"No instance of {typeof(T).Name} found in the project");
+#if UNITY_EDITOR
+                        if(!AssetDatabase.IsValidFolder("Assets/Resources"))
+                            AssetDatabase.CreateFolder("Assets", "Resources");
+
+                        _instance = CreateInstance<T>();
+                        string assetPath = AssetDatabase.GenerateUniqueAssetPath($"Assets/Resources/{typeof(T).Name}.asset");
+                        AssetDatabase.CreateAsset(_instance, assetPath);
+
+                        AssetDatabase.SaveAssets();
+                        AssetDatabase.Refresh();
+#endif
+                    }
+                }
+                return _instance;
+            }
+        }
+
+#if UNITY_EDITOR
+        protected static void PingSO()
+        {
+            EditorUtility.FocusProjectWindow();
+            Selection.activeObject = Instance;
+            EditorGUIUtility.PingObject(Instance);
+        }
+#endif
+    }
+}
